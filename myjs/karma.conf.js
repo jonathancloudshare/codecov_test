33514fa4a33eb7ad5720b5b4fb22679db2e067e0
jonathancloudshare <jonathan@cloudshare.com>
2018-06-17 14:17:38 +0300

    added coverage code

--- myjs/karma.conf.js
+++ myjs/karma.conf.js
@@ -36,8 +36,11 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage-istanbul'],
 
+    coverageIstanbulReporter: {
+        reports: ["lcovonly"]
+    },
 
     // web server port
     port: 9876,
--- myjs/webpack.spec.config.js
+++ myjs/webpack.spec.config.js
@@ -3,5 +3,15 @@ var webpackConfig = require('./webpack.config');
 
 webpackConfig.entry = {bundle: "./index.spec"};
 webpackConfig.output.path = path.join(__dirname, "./distSpec");
-
+webpackConfig.module.rules.push({
+    test: /\.jsx?$/,
+    use: {
+        loader: 'istanbul-instrumenter-loader',
+        options: { 
+            esModules: true 
+        }
+    },
+    enforce: 'post',
+    exclude: /node_modules|\.spec\.js$/,
+});
 module.exports = webpackConfig;
