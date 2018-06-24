2fc5c178ce799eed83f22a1ef55499dc627c7c95
jonathancloudshare <jonathan@cloudshare.com>
2018-06-24 12:02:51 +0300

    fix paths and stuff

--- MyCSharp/ci/test_coverage.cmd
+++ MyCSharp/ci/test_coverage.cmd
@@ -2,10 +2,9 @@ REM choco install -y opencover.portable
 REM choco install -y codecov
 opencover.console ^
     -target:"C:\ProgramData\chocolatey\bin\nunit3-console.exe" ^
-    -targetargs:"tests.nunit" ^
+    -targetargs:"MyCSharp\ci\tests.nunit" ^
     -filter:"+[ClassLibrary1]* -[ClassLibrary1Test]*" ^
     -register:user ^
     -output:"CoverageResult.xml"
 
-codecov --token "%1" ^
-        --file "CoverageResult.xml"
+codecov --file "CoverageResult.xml"
